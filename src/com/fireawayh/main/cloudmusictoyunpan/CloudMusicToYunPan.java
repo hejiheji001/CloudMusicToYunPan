63583bdff1f8cdb76ed827eca284f7bd290777e1
hejiheji001@gmail.com <hejiheji001@gmail.com>
2016-01-17 23:18:27 +0000

    Init

--- src/com/fireawayh/main/cloudmusictoyunpan/CloudMusicToYunPan.java
+++ src/com/fireawayh/main/cloudmusictoyunpan/CloudMusicToYunPan.java
@@ -27,6 +27,7 @@ public class CloudMusicToYunPan {
 
         String username = "";
         String password = "";
+        String path = "/";
 
         if(argList.contains("-u") && argList.contains("-p")) {
             username = argList.get(argList.indexOf("-u") + 1).toString();
@@ -43,6 +44,7 @@ public class CloudMusicToYunPan {
                     prop.load(new FileInputStream(file));
                     username = prop.getProperty("Username");
                     password = prop.getProperty("Password");
+                    path = prop.getProperty("Savepath");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -53,15 +55,19 @@ public class CloudMusicToYunPan {
             YunOffline yo = new YunOffline(username, password);
             if(yo.initYunPan()){
                 yo.getYunPanToken();
+                if(argList.contains("-path")) {
+                    path = argList.get(argList.indexOf("-path") + 1).toString().toUpperCase();
+                }
+
                 if(argList.contains("-playlist")) {
                     String playListId = argList.get(argList.indexOf("-playlist") + 1).toString().toUpperCase();
                     System.out.println("Saved Playlist " + playListId + " To Yun Pan");
-                    saveListToYunPan(yo, playListId);
+                    saveListToYunPan(yo, playListId, path);
                 }
                 if(argList.contains("-id")) {
                     String songId = argList.get(argList.indexOf("-id") + 1).toString().toUpperCase();
                     System.out.println("Saved Song " + songId + " To Yun Pan");
-                    saveToYunPan(yo, songId);
+                    saveToYunPan(yo, songId, path);
                 }
             }else{
                 System.err.println("Init Failed");
@@ -69,7 +75,7 @@ public class CloudMusicToYunPan {
         }
     }
 
-    public static void saveToYunPan(YunOffline yo, String id){
+    public static void saveToYunPan(YunOffline yo, String id, String path){
         MusicUtils mu = new MusicUtils(id);
         Map music = mu.getBestMusic().object();
         String artistName = mu.getArtist();
@@ -80,6 +86,7 @@ public class CloudMusicToYunPan {
         String durl = au.getDownloadUrl(bestMusicId);
         System.out.println("Current task: " + newFileName);
         yo.setSource_url(durl);
+        yo.setSavepath(path);
         yo.saveToYunPan("", "");
 //        if(!taskid.isEmpty()){
 //            System.out.println("Saved TO Yun Pan, ID IS " + taskid);
@@ -88,10 +95,10 @@ public class CloudMusicToYunPan {
 //        }
     }
 
-    public static void saveListToYunPan(YunOffline yo, String listId){
+    public static void saveListToYunPan(YunOffline yo, String listId, String path){
         ArrayList<String> songIds = ju.getPlayListSongs(listId);
         for (int i = 0; i < songIds.size() - 1; i++){
-            saveToYunPan(yo, songIds.get(i));
+            saveToYunPan(yo, songIds.get(i), path);
         }
     }
 
@@ -116,6 +123,7 @@ public class CloudMusicToYunPan {
         System.out.println("-u <baidu yun username> -p <baidu yun password> [options]");
         System.out.println("-c <userinfo.properties> [options]");
         System.out.println("-u <baidu yun username> -p <baidu yun password> [options]");
+        System.out.println("-path <save path of baidu yun>");
         System.out.println("-id <netease music id>");
         System.out.println("or");
         System.out.println("-playlist <netease music play list id>");
